6d523028c502d8377bfdab9b9838da33844bca12
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-06-17 21:56:55 +0200

    Add SSML class

---
+++ source/EasyCloud/Services/TextToSpeech/SpeechSynthesisMarkupLanguage.cs
@@ -0,0 +1,16 @@
+﻿namespace EasyCloud.Services.TextToSpeech
+{
+    public class SpeechSynthesisMarkupLanguage
+    {
+        public string Text { get; }
+        public Voice Voice { get; }
+
+        public SpeechSynthesisMarkupLanguage(Voice voice, string text) => (Voice, Text) = (voice, text);
+
+        public string ToXml() => $"<speak version='1.0' xmlns='https://www.w3.org/2001/10/synthesis' " +
+            $"xml:lang='{Voice.Locale.ToString().Replace("_", "-")}'>" +
+            $"<voice name='{Voice.ServiceName}'>{Text}</voice></speak>";
+
+        public override string ToString() => ToXml();
+    }
+}
---
+++ source/EasyCloudTest/Services/TextToSpeech/SpeechSynthesisMarkupLanguageTest.cs
@@ -0,0 +1,27 @@
+﻿using EasyCloud.Services.TextToSpeech;
+using LucidCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace EasyCloudTest.Services.TextToSpeech
+{
+    [TestClass]
+    public class SpeechSynthesisMarkupLanguageTest
+    {
+        const Locale ExpectedLocale = Locale.pl_PL;
+        const string ExpectedXml = "<speak version='1.0' xmlns='https://www.w3.org/2001/10/synthesis' " +
+            "xml:lang='pl-PL'><voice name='" + VoiceName + "'>" + Text + "</voice></speak>";
+        const string Text = "TEXT";
+        const string VoiceName = "VOICE-NAME";
+
+        [TestMethod]
+        public void Expected_Xml() => LucidTest
+            .Arrange(() => new Voice(ExpectedLocale, "", Gender.Female, VoiceName))
+            .Act(voice => new SpeechSynthesisMarkupLanguage(voice, Text))
+            .Assert(ssml =>
+            {
+                ssml.ToXml().ShouldBe(ExpectedXml);
+                ssml.ToString().ShouldBe(ExpectedXml);
+            });
+    }
+}
