2e20bd080e500dd017092d9ad1462c4605e65d32
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-06-19 23:52:47 +0200

    Add simple speak example

---
+++ Examples/Text-to-speech/AI.TextToSpeech.Simple/Program.cs
@@ -0,0 +1,15 @@
+﻿using EasyCloud;
+using EasyCloud.WindowsOnly;
+using System.Threading.Tasks;
+
+namespace AI.TextToSpeech.Simple
+{
+    public class Program
+    {
+        public static Task Main() => Region.WestEurope
+            .GetCognitiveService("put-cognitive-service-key-here")
+            .SpeakEnglishWomanAsync("Hello World!")
+            .SpeakEnglishManAsync("Hello World!")
+            .SpeakPolishAsync("Witaj świecie!");
+    }
+}
